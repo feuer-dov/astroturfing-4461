69bf54fd4c95dadbb5396180feddaccf183ec002
feuer-dov <77290738+feuer-dov@users.noreply.github.com>
2025-03-26 01:35:55 -0400

    content moderator added
    added a content moderator and total bot and human likes adjust when post is taken down

--- src/visualize.py
+++ src/visualize.py
@@ -2,31 +2,38 @@ from mesa.visualization import make_plot_component, make_space_component, Solara
 from model import AstroturfingModel
 from agent import HumanUser, PostAgent, BotAmplifier
 
+# Default values
+num_humans = 85
+num_bots = 25
+num_posts = 10
+
 def astroturfing_portrayal(agent):
     if isinstance(agent, HumanUser):
         return {"color": "blue", "size": 30, "layer": 1}
     elif isinstance(agent, PostAgent):
         size = agent.get_visual_size()
-        return {"color": "red", "size": size, "layer": 0}
+        color = "red" if agent.removed else "orange"
+        return {"color": color, "size": size, "layer": 0}
 
 space_component = make_space_component(astroturfing_portrayal, draw_grid=False)
+
 chart_component = make_plot_component({
     "Total_Bot_Likes": "green",
-    "Total_Human_Likes": "blue"
+    "Total_Human_Likes": "blue",
 })
 
 model_params = {
-    "num_bots": Slider("Number of Bots", 20, 0, 100, 1),
-    "num_humans": Slider("Number of Humans", 20, 1, 100, 1),
-    "num_posts": Slider("Number of Posts", 5, 1, 20, 1),
-    "grid_size": Slider("Grid Size", 10, 10, 150, 10),
+    "num_bots": Slider("Number of Bots", num_bots, 0, 100, 1),
+    "num_humans": Slider("Number of Humans", num_humans, 1, 100, 1),
+    "num_posts": Slider("Number of Posts", num_posts, 1, 20, 1),
+    "grid_size": Slider("Grid Size", 100, 10, 150, 10),
 }
 
 model = AstroturfingModel(
-    num_bots=20,
-    num_humans=20,
-    num_posts=5,
-    grid_size=10
+    num_bots=num_bots,
+    num_humans=num_humans,
+    num_posts=num_posts,
+    grid_size=100
 )
 
 page = SolaraViz(
